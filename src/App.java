401a981310935f316cda4231b78aa900fcc69117
HueiYingWang <s89086guy@gmail.com>
2022-07-16 16:47:05 +0800

    Wiin partial solution

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.function.Function;
+
 public class App {
+      
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    int balance;
+    int newbalance;
+    int amount;
+    
+    newbalance = returnADeposit(0,100);
+    System.out.println(newbalance);
+       
     }
+    public static int returnAWithdraw(int balance, int amount) {
+        int newbalance = balance-amount;
+        return newbalance;
+      }
+    public static int returnADeposit(int balance, int amount) {
+        int newbalance = balance+amount;
+        return newbalance;
+      }
+    public static int returnATransfer(int balance, int amount) {
+        int newbalance = balance-amount;
+        return newbalance;
+      }
+    
 }
