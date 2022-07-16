ddb3000d9e5dc6680c4377d9dabb493d5512ddba
HueiYingWang <s89086guy@gmail.com>
2022-07-16 17:04:09 +0800

    refactor

--- src/App.java
+++ src/App.java
@@ -1,28 +1,31 @@
-import java.util.function.Function;
-
 public class App {
       
-
     public static void main(String[] args) throws Exception {
-    int balance;
-    int newbalance;
-    int amount;
-    
-    newbalance = returnADeposit(0,100);
-    System.out.println(newbalance);
-       
+        int balance = 0;
+        System.out.println(balance);
+
+        balance = deposit(balance, 5000);
+        System.out.println(balance);
+
+        balance = withdraw(balance, 100);
+        System.out.println(balance);
+        
+        balance = transfer(balance, 10);
+        System.out.println(balance);      
     }
-    public static int returnAWithdraw(int balance, int amount) {
-        int newbalance = balance-amount;
+
+    public static int deposit(int balance, int amount) {
+        int newbalance = balance + amount;
         return newbalance;
-      }
-    public static int returnADeposit(int balance, int amount) {
-        int newbalance = balance+amount;
+    }
+
+    public static int withdraw(int balance, int amount) {
+        int newbalance = balance - amount;
         return newbalance;
-      }
-    public static int returnATransfer(int balance, int amount) {
-        int newbalance = balance-amount;
+    }
+
+    public static int transfer(int balance, int amount) {
+        int newbalance = balance - amount;
         return newbalance;
-      }
-    
+    }
 }
